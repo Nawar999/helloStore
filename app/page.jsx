9452fd90b82e9969335cdf7a4aae34621958594d
Nawar999 <nawar533398@gmail.com>
2024-07-14 03:47:23 +0300

    [commit message]

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
-
+import { auth } from "@clerk/nextjs/server";
 const HomePage = async () => {
+  const user = auth();
+
+  console.log(user);
   return (
     <div>
       <Button>Click</Button>
